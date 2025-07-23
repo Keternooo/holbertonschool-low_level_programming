a7cf55fec685c7e5813bb7094da2d70ab44b9095
root <keterno@proton.me>
2025-07-23 14:16:23 +0000

    Third Day

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen - funciton that return the length of a string
- * @s: the string that len will be return
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
-	return strlen(*s)
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
