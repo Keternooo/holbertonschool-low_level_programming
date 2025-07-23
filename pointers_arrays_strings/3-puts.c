44b9e72cd10d57f12f4e6b45b2f5900711f4d617
root <keterno@proton.me>
2025-07-23 14:22:07 +0000

    Third Day

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ * @str:  string to evaluate
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", *str);
+}
