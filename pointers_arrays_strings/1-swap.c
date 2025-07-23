46dc2c6be26bd9b47d9b83405d28e7dc970cba21
root <keterno@proton.me>
2025-07-23 14:09:13 +0000

    Third Day

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * swap_int function thats swaps the values of two intergers.
- * @a fisrt int
- * @b second int
+ * swap_int - function thats swaps the values of two intergers.
+ * @a: fisrt int
+ * @b: second int
  */
 
 void swap_int(int *a, int *b)
