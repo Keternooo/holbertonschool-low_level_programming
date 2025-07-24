a0973ff8c1ec469ef4a62a93203508cbf88518e4
root <keterno@proton.me>
2025-07-24 11:07:56 +0000

    Fourth Day

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes;
-	
+	size_t nodes = 0;
+
 	while (h)
 	{
 		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
+
+	printf("-> %zu elements\n", nodes);
 
 	return (nodes);
 }
