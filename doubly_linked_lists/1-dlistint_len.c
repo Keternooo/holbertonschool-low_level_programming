a8ac5317bb920c8499876a25d0f39e110447c361
root <keterno@proton.me>
2025-07-24 11:21:29 +0000

    Fourth Day

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * dlistint_len - function that returns the number of elements in a linked dlistint_t list.
+ * dlistint_len - function that returns the number
+ * of elements in a linked dlistint_t list.
  * @h: the linked element to count
  * Return: The length of linked element
  */
@@ -9,6 +10,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	int len = 0;
+
 	while (h)
 	{
 		h = h->next;
