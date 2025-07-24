53eb27fb38f008bf8fe8ed28d9d53124077a4729
root <keterno@proton.me>
2025-07-24 14:28:21 +0000

    Fourth Day

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 
-	if(tmp->next == NULL)
+	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
