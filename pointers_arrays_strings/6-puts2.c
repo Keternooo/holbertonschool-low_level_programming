f6c1a5605d8630dbb0f11959044adf8b37b267ce
root <keterno@proton.me>
2025-07-24 08:22:40 +0000

    Fourth Day

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - that prints every other character of a string, starting with the first character, followed by a new line.
+ * @str: string to evaluate
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i])
+		i += 2;
+	}
+
+}
