abb1a47e2f2abe1e57916a3e3d6940b2e28004ea
root <keterno@proton.me>
2025-07-24 08:48:01 +0000

    Fourth Day

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
-		_putchar(',');
-		_putchar(' ');
+		if (i == 0) printf("%d", a[i]);
+		else printf(", %d", a[i]);
 	}
 
 }
