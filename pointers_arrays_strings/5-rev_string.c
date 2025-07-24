d663bc03347cff43a71d43a7278eda37a6278ed8
root <keterno@proton.me>
2025-07-24 08:11:28 +0000

    Fourth Day

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,18 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int j;
-	char strRev = '';
+	char temp;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	
+	while (i < j)
+	{
+    		temp = s[i];
+	    	s[i] = s[j];
+   		s[j] = temp;
+    		i++;
+	    j--;
+	}
 
 }
