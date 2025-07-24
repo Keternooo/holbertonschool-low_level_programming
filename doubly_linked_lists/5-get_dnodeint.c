12f1c6237718d394a1e57af3b641c07dcdfbc59b
root <keterno@proton.me>
2025-07-24 13:01:09 +0000

    Fourth Day

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *tmp;
-	int i = 0;
+	unsigned int i = 0;
 
 	tmp = head;
 
@@ -24,5 +24,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		i++;
 	}
 
-	return (NULL)
+	return (NULL);
 }
