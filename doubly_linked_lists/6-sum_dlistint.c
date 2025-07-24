936d535b6d110e99fb6402962f72d85ba325aadb
root <keterno@proton.me>
2025-07-24 13:10:13 +0000

    Fourth Day

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of
+ * all the data (n) of a dlistint_t linked list.
+ * @head: The list to evaluate
+ * Return: 0 or the sum of all the data
+ */
+
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next
+	}
+
+	return (sum);
+}
