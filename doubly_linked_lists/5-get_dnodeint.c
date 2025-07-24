e1b8e2c7c61848acd5da4cf3157edf863f26cd8a
root <keterno@proton.me>
2025-07-24 13:02:30 +0000

    Fourth Day

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - function that returns the nth node of a dlistint_t linked list.
+ * get_dnodeint_at_index - function that returns
+ * the nth node of a dlistint_t linked list.
  * @head: My list
  * @index: The integer for searching index
  * Return: NULL or the list index
